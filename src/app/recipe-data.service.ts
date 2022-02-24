3a86cc8d5f0abf40f5a65a3dfc766b4b9cad00f0
MRSS <mateusyasd@hotmail.com>
2022-02-24 01:14:26 -0300

    fixed route bug

--- src/app/recipe-data.service.ts
+++ src/app/recipe-data.service.ts
@@ -11,7 +11,29 @@ export class RecipeDataService {
 
   constructor() {
     this.recipes = [{ name: "Bolo de Fubá",
-      description:"Just do it bro",
+      description:`
+      Ingredientes
+      
+          3 ovos
+          1 xícara de chá de óleo
+          1 xícara de chá de leite
+          2 xícaras de chá de açúcar
+          1 xícara de chá de farinha de trigo
+          2 xícaras de chá de fubá
+          1 colher de sopa de fermento em pó
+      
+      Modo de Preparo
+      
+          1. Peneirar os secos numa tigela e reservar.
+          2. Bater os líquidos no liquidificador e juntar nos secos peneirados.
+          3. Colocar em forma de anel, ou outra de sua preferência, untada e polvilhada com fubá.
+          4. Forno aproximadamente 30 a 40 minutos. 
+      
+      Dicas:
+      
+          Eu não uso o liquidificador; bato bem com o fuê.
+          Tamanho das formas que uso para esse bolo: 23 centímetros de diâmetro por 8 centímetros de altura ou 25 centímetros de diâmetro por 7 centímetros de altura.
+          Para assar coloco a forma de anel dentro de outra forma redonda pois, no caso de transbordar, não suja o forno, e este bolo cresce bastante.`,
       image: "https://img.itdg.com.br/tdg/images/recipes/000/009/254/2358/2358_original.jpg?mode=crop&width=710&height=400",
       id: 0
     }]
